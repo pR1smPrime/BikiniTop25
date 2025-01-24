e8c603ee5840f89eb50a3e40c9b1fe849aa1f428
pR1smPrime <niklas.weigert1989@gmail.com>
2025-01-25 00:24:44 +0100

    tweaking physics

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -100.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 0.5
+		velocity += get_gravity() * delta * 0.1
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
