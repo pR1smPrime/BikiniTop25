98ee2f3a8e7b33a534f2fe5858b65b8ce0b5c83d
Meimarkter <bastiankopf@gmail.com>
2025-01-25 13:55:12 +0100

    player animation

--- scenes/player/character_body_2d.gd
+++ scenes/player/character_body_2d.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -100.0
-
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
+	#Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 0.1
+		sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,3 +29,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	if Input.is_action_just_pressed('ui_left'):
+
+		sprite_2d.flip_h = true
+
+	if Input.is_action_just_pressed('ui_right'):
+
+		sprite_2d.flip_h = false
+	
