7ed47eb51d359430b978d61a71be2804bde427a9
pR1smPrime <niklas.weigert1989@gmail.com>
2025-01-25 16:47:40 +0100

    added level select

--- scenes/mainmenue/main_menu.gd
+++ scenes/mainmenue/main_menu.gd
@@ -5,7 +5,9 @@ extends Control
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
-
+	
+func _on_level_select_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/mainmenue/level_select.tscn") # Replace with function body.
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
