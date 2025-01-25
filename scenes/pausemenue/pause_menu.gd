518d3ee148e4bc4fc67443cd2d04805c081e91f3
Meimarkter <bastiankopf@gmail.com>
2025-01-25 22:31:05 +0100

    retry and "Tutorial"

--- scenes/pausemenue/pause_menu.gd
+++ scenes/pausemenue/pause_menu.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @onready var pause_panel: Panel = $PausePanel
+@onready var intro: Panel = $Intro
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,9 +21,14 @@ func _on_resume_pressed() -> void:
 	get_tree().paused = false	
 
 func _on_retry_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	var _reload = get_tree().reload_current_scene()
+	
 
 
 func _on_exit_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://bikini_toppem_moving_service.tscn")
+
+func _on_button_button_down() -> void:
+	intro.hide()
