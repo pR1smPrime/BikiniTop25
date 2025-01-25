ca9b427de20fa6ec4e73b890a883b9d34b269e4f
pR1smPrime <niklas.weigert1989@gmail.com>
2025-01-25 18:37:39 +0100

    change collion for objects, create level 2

--- scenes/mainmenue/level_select.gd
+++ scenes/mainmenue/level_select.gd
@@ -12,4 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_level_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
