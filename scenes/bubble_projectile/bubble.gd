71a8ace5fcbb3a9c6d8b1cada3dd2383a30fd430
Volkerolidiot <janchristianhopp@gmail.com>
2025-01-25 20:23:52 +0100

    bubbles

--- scenes/bubble_projectile/bubble.gd
+++ scenes/bubble_projectile/bubble.gd
@@ -1,15 +1,20 @@
 extends RigidBody2D
+class_name Bubble
 
+@export var time_to_pop := 1.5
 
 func init(bubble_velocity : Vector2) -> void:
 	linear_velocity = bubble_velocity
-	
-# Called when the node enters the scene tree for the first time.
+	$TimeToPop.wait_time = time_to_pop
+
 func _ready() -> void:
 	$Bubble.play()
+	$TimeToPop.wait_time = time_to_pop
 
-func _on_time_to_pop_timeout() -> void:
+func pop_bubble() -> void:
 	# pop animation
 	$Pop.play()
-	
 	queue_free()
+
+func _on_time_to_pop_timeout() -> void:
+	pop_bubble()
