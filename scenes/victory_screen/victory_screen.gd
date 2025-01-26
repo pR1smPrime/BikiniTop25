b3b32538d7b5d981aa10e20315b15839e5b7bce8
Thilo Renner <thilorenner2001@gmail.com>
2025-01-26 14:57:35 +0100

    fix next level

--- scenes/victory_screen/victory_screen.gd
+++ scenes/victory_screen/victory_screen.gd
@@ -1,9 +1,8 @@
 extends Control
 
-
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenue/main_menu.tscn") # Replace with function body.
 
 
 func _on_next_level_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level" + str(int(get_tree().current_scene.scene_file_path) + 1 ) + ".tscn") 
+	get_tree().change_scene_to_file("res://scenes/levels/level" + str(int(PreviousScene.scene) + 1 ) + ".tscn") 
