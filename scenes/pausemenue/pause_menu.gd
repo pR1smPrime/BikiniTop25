ea87773db78b40956582aabc12dc33c2fc763ae9
Meimarkter <bastiankopf@gmail.com>
2025-01-26 11:01:25 +0100

    tut screen update

--- scenes/pausemenue/pause_menu.gd
+++ scenes/pausemenue/pause_menu.gd
@@ -3,10 +3,13 @@ extends Node
 @onready var pause_panel: Panel = $PausePanel
 @onready var intro: Panel = $Intro
 
+func _init() -> void:
+	pass
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	intro.show()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
