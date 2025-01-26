5c841777067227aae42985f37d5bc84a57405804
pR1smPrime <niklas.weigert1989@gmail.com>
2025-01-26 15:37:57 +0100

    fixed labels AGAIN

--- assets/objects/statics/area_2d.gd
+++ assets/objects/statics/area_2d.gd
@@ -1,17 +1,17 @@
 extends Area2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var moveable : Moveable
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _process(_delta: float) -> void:
+	if moveable != null:
+		if overlaps_body(moveable):
+			if moveable.linear_velocity.length() < 0.0001:
+				get_tree().change_scene_to_file("res://scenes/victory_screen/victory_screen.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Moveable:
-		PreviousScene.scene = get_tree().current_scene.name
-		get_tree().change_scene_to_file("res://scenes/victory_screen/victory_screen.tscn")
-	
+		moveable = body
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == moveable:
+		moveable = null
