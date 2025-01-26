9b26a790ed08e873d587394da2dab4ca5da52c87
Meimarkter <bastiankopf@gmail.com>
2025-01-26 13:39:46 +0100

    Credits added

--- scenes/mainmenue/main_menu.gd
+++ scenes/mainmenue/main_menu.gd
@@ -12,5 +12,8 @@ func _on_level_select_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits/credits.tscn")
+
 #func _ready():
 #	$AnimationPlayer.play("logo_Wiggle")
