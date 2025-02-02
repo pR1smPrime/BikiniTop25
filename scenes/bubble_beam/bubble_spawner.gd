c04e3c4b0b2822741641be2a2f45d299b3226a4c
Volkerolidiot <janchristianhopp@gmail.com>
2025-02-02 01:14:38 +0100

    neues level_chris_1.gd

--- scenes/bubble_beam/bubble_spawner.gd
+++ scenes/bubble_beam/bubble_spawner.gd
@@ -1,9 +1,10 @@
 extends Node2D
-class_name BubbleBeamer
+class_name BubbleSpawner
 
 @export var force := 1500.0
 @export var shoot_frequence := 0.005
 @export var time_to_pop := 1.5
+@export var direction : Vector2
 
 @onready var bubble_node := preload("res://scenes/bubble_projectile/bubble.tscn")
 @onready var shoot_timer := $ShootFrequency
@@ -15,12 +16,3 @@ func spawn_bubble(direction: Vector2):
 		direction = direction.rotated(randf_range(-0.2, 0.2))
 		bubble.init(direction * force, time_to_pop)
 		shoot_timer.start(shoot_frequence)
-
-func _process(_delta: float) -> void:
-	if Input.is_action_pressed("LeftMouse"):
-		var direction := position.direction_to(get_local_mouse_position())
-		spawn_bubble(direction)
-	if Input.get_vector("aim_left_gamepad", "aim_right_gamepad", "aim_up_gamepad", "aim_down_gamepad"):
-		var direction := Input.get_vector("aim_left_gamepad", "aim_right_gamepad", "aim_up_gamepad", "aim_down_gamepad")
-		spawn_bubble(direction)
-	
